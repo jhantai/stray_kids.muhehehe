c9550049dbd5c70dee3305336b09c19dad3a4a76
Daniel <iapyzov8@gmail.com>
2024-03-03 02:42:24 +0600

    Swiper

--- src/constants/animalsSwiper.js
+++ src/constants/animalsSwiper.js
@@ -2,63 +2,60 @@ import cat1 from '../assets/cat-barsik.jpg'
 
 export const animalsSwiper = [
   {
-    name: 'Кот Барсик',
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+   
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+   
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+  
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    
   },
   {
-    name: 'Кот Барсик',
+    
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+      },
+  {
+    
+    src: cat1,
+      },
+  {
+    
+    src: cat1,
+   
   },
   {
-    name: 'Кот Барсик',
+   
     src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
-  },
-  {
-    name: 'Кот Барсик',
-    src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
-  },
-  {
-    name: 'Кот Барсик',
-    src: cat1,
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+   
   }
 ]
--- src/modules/SwiperAnimals/SwiperAnimals.jsx
+++ src/modules/SwiperAnimals/SwiperAnimals.jsx
@@ -7,9 +7,10 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './SwiperAnimals.css'
 
+
 export default function App() {
   return (
-    <>
+    <><h3 className='title'>Наши животные</h3>
       <Swiper
         navigation={true}
         pagination={true}
@@ -19,11 +20,11 @@ export default function App() {
         {animalsSwiper.map((item, idx) => {
           return (
               <SwiperSlide key={idx}>
-                <h3>{item.name}</h3>
+                
                 <div>
-                  <img src={item.src} alt="cat"/>
+                  <img  src={item.src} alt="cat"/>
                 </div>
-                <p className='swiper-animals-description'>{item.description}</p>
+                {/* <p className='swiper-animals-description'>{item.description}</p> */}
               </SwiperSlide>
           )
         })}
