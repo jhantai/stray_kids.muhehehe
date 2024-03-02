c1b853b0c1cd45aba0e521fb970816410bf78859
ArtemChubur <artemchubur@gmail.com>
2024-03-02 14:35:27 +0600

    's'

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TitleSection from "../../modules/TitleSection/TitleSection";
 import Advantages from "../../modules/Advantages/Advantages";
+import Grooming from "../../modules/Grooming/Grooming";
 
 const HomePage = () => {
     return (
         <div style={{overflow: "hidden"}}>
            <TitleSection />
-            <Advantages />
+           <Advantages />
+           <Grooming/>
         </div>
     );
 };
