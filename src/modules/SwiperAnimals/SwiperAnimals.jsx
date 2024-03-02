5e8859ab91b41618252d15ff98d6b97141a9864f
oscarquell <oscarquell96@gmail.com>
2024-03-02 19:03:02 +0600

    added swiper

--- src/modules/SwiperAnimals/SwiperAnimals.jsx
+++ src/modules/SwiperAnimals/SwiperAnimals.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import './SwiperAnimals.css'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import {animalsSwiper} from "../../constants/animalsSwiper";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './SwiperAnimals.css'
 
-const SwiperAnimals = (props) => {
+export default function App() {
   return (
-    <Swiper
-    spaceBetween={50}
-    slidesPerView={3}
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
-  >
-    <SwiperSlide>Slide 1</SwiperSlide>
-    <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
-    ...
-  </Swiper>
-  )
+    <>
+      <Swiper
+        navigation={true}
+        pagination={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {animalsSwiper.map((item, idx) => {
+          return (
+              <SwiperSlide key={idx}>
+                <h3>{item.name}</h3>
+                <div>
+                  <img src={item.src} alt="cat"/>
+                </div>
+                <p className='swiper-animals-description'>{item.description}</p>
+              </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </>
+  );
 }
-
-export default SwiperAnimals
